06a74dbafb11be1ae2017cacbe4bc88bb71b2eab
ipads280 <ipads280@icloud.com>
2022-07-16 16:38:47 +0000

    override

--- game/Hero.java
+++ game/Hero.java
@@ -1,27 +1,16 @@
 public class Hero {
-    String name;
-    int hp;
+    String name="ミナト";
+    int hp=100;
     
-    public void sleep() {
-        this.hp=100;
-        System.out.println(this.name+"は、眠って回復した!");
+    // 戦う
+    public void attack(Matango m) {
+        System.out.println(this.name+"の攻撃！");
+        m.hp-=5;
+        System.out.println("5ポイントのダメージをあたえた！");
     }
     
-    public void sit(int sec) {
-        this.hp+=sec;
-        System.out.println(this.name+"は、"+sec+"秒座った!");
-        System.out.println("HPが"+sec+"ポイント回復した");
-    }
-    
-    public void slip() {
-        this.hp-=5;
-        System.out.println(this.name+"は、転んだ!");
-        System.out.println("5のダメージ");
-    }
-    
+    // 逃げる
     public void run() {
-        System.out.println(this.name +"は、逃げ出した!");
-        System.out.println("GAMEOVER");
-        System.out.println("最終HPは"+this.hp+"でした");
+        System.out.println(this.name+"は逃げ出した!");
     }
 }
--- game/Main.java
+++ game/Main.java
@@ -1,24 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        // １，勇者を生成
-        Hero h =new Hero();
+        SuperHero sh=new SuperHero();
+        sh.run();
         
-        // 2,フィールドに初期値をセット
-        h.name="ミナト";
-        h.hp=100;
-        System.out.println("勇者"+h.name+"を生み出しました!");
-        
-        Matango m1 =new Matango();
-        m1.hp=50;
-        m1.suffix='A';
-        
-        Matango m2 =new Matango();
-        m2.hp=48;
-        m1.suffix='B';
-        
-        h.slip();
-        m1.run();
-        m2.run();
+        Hero h=new Hero();
         h.run();
     }
 }
