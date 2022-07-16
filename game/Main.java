2acaf010f12390b6de2e8d30ad0b362488904b51
ipads280 <ipads280@icloud.com>
2022-07-16 11:33:31 +0000

    第8章

---
+++ game/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        // １，勇者を生成
+        Hero h =new Hero();
+        
+        // 2,フィールドに初期値をセット
+        h.name="ミナト";
+        h.hp=100;
+        System.out.println("勇者"+h.name+"を生み出しました!");
+        
+        Matango m1 =new Matango();
+        m1.hp=50;
+        m1.suffix='A';
+        
+        Matango m2 =new Matango();
+        m2.hp=48;
+        m1.suffix='B';
+        
+        h.slip();
+        m1.run();
+        m2.run();
+        h.run();
+    }
+}
