27d430ef95d25d71f4002567d448f3677db8447b
ipads280 <ipads280@icloud.com>
2022-07-17 05:33:19 +0000

    10章

--- game/Main.java
+++ game/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        SuperHero sh=new SuperHero();
-        sh.run();
-        
-        Hero h=new Hero();
-        h.run();
+        SuperHero sh = new SuperHero();
     }
 }
--- game/SuperHero.java
+++ game/SuperHero.java
@@ -13,4 +13,19 @@ public class SuperHero extends Hero {
     public void run() {
         System.out.println(this.name+"は徹底した");
     }
+    
+    public void attack(Matango m) {
+        System.out.println(this.name+"の攻撃!");
+        m.hp-=5;
+        System.out.println("5ポイントのダメージを与えた!");
+        if (this.flying) {
+            System.out.println(this.name+"の攻撃!");
+            m.hp-=5;
+            System.out.println("5ポイントのダメージを与えた");
+        }
+    }
+    
+    public  SuperHero() {
+        System.out.println("SuperHeroのコンストラクタが動作");
+    }
 }
