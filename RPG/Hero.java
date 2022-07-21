d189ca8d07dba93de4f2d2eeee731ed417bb16c6
ipads280 <ipads280@icloud.com>
2022-07-21 04:55:44 +0000

    インターフェイス

--- RPG/Hero.java
+++ RPG/Hero.java
@@ -1,27 +1,6 @@
-public class Hero {
-    String name;
-    int hp;
-    
-    public void sleep() {
-        this.hp=100;
-        System.out.println(this.hp+"は、眠って回復した!");
-    }
-    
-    public void sit(int sec) {
-        this.hp+=sec;
-        System.out.println(this.name+"は、"+sec+"秒座った!");
-        System.out.println("HPが"+sec+"ポイント回復した");
-    }
-    
-    public void slip() {
-        this.hp-=5;
-        System.out.println(this.name+"は、転んだ！");
-        System.out.println("5のダメージ");
-    }
-    
-    public void run() {
-        System.out.println(this.name+"は、逃げ出した");
-        System.out.println("GAMEOVER");
-        System.out.println("最終HPは"+this.hp+"でした");
+public class Hero extends Character {
+    public void attack(Matango m) {
+        System.out.println(this.name+"の攻撃");
+        System.out.println("敵に10ポイントのダメージを与えた")
     }
 }
