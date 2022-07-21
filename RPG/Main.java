d5b195a7a9e8ba4896d670627c4b668d2d71fb17
ipads280 <ipads280@icloud.com>
2022-07-21 08:45:16 +0000

    第12章課題終わり

--- RPG/Main.java
+++ RPG/Main.java
@@ -1,21 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Hero h=new Hero();
-        h.name="ミナト";
-        h.hp=100;
-        
-        Matango m1=new Matango();
-        m1.hp=50;
-        m1.suffix='A';
-        
-        Matango m2=new Matango();
-        m2.hp=48;
-        m2.suffix='B';
-        
-        h.slip();
-        m1.run();
-        m2.run();
-        h.run();
-        
+        Monster[] monsters=new Monter[3];
+        monsters[0]=new Slime();
+        monsters[1]=new Goblin();
+        mosnters[2]=new DeathBat();
+        for (Mosnter m :monsters) {
+            m.run();
+        }
     } 
 }
